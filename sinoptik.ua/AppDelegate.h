5c1fd71c04a73bcbf1fe00ed93469e4dc3d5d61d
Vasiliy Horbachenko <shadow.prince@ya.ru>
2015-09-07 12:06:00 +0300

    Initial Commit

---
+++ sinoptik.ua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sinoptik.ua
+//
+//  Created by shdwprince on 9/7/15.
+//  Copyright (c) 2015 shdwprince. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
